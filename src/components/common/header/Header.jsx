2e1c9fa6ae867825f110c10d1b7e6119e05d74b7
erikdalgrund <92083787+erikdalgrund@users.noreply.github.com>
2022-12-20 20:04:10 +0100

    examen[003] Chore: changed header structure

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,16 +1,17 @@
 import React from "react"; 
+import '../../../sass/styles.scss';
 
 function Header() {
     return (
-        <div>
-            <h2>
+        <div class="header">
+            <h2 class="header__title">
                 LoL Corporate
             </h2>
-            <ul>
-                <li>Home</li>
-                <li>Cart</li>
-                <li>Shop</li>
-                <li>About</li>
+            <ul class="header__navbar">
+                <li><a href="#" class="header__navbar--items">Home</a></li>
+                <li><a href="#" class="header__navbar--items">Cart</a></li>
+                <li><a href="#" class="header__navbar--items">Shop</a></li>
+                <li><a href="#" class="header__navbar--items">About</a></li>
             </ul>
         </div>
     )
