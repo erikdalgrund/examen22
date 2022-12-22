d806ae949c0e9f61cd3273a424a3d2466ee9ffb9
erikdalgrund <92083787+erikdalgrund@users.noreply.github.com>
2022-12-22 18:48:01 +0100

    examen[008] Chore: Fixed a working carousel component.

--- src/components/common/carousel/Carousel.jsx
+++ src/components/common/carousel/Carousel.jsx
@@ -1,19 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Carousel( {images} ) {
+    
+    const [current, setCurrent] = useState(0);
+    const [autoPlay, setAutoPlay] = useState(true);
+    let timeOut = null;
+
+    useEffect(() => {
+        timeOut = 
+        autoPlay && setTimeout(() => {
+            slideRight();
+        }, 4000);
+    });
+
+    const slideRight = () => {
+        setCurrent(current === images.length -1 ? 0 : current + 1);
+    };
+
+    const slideLeft = () => {
+        setCurrent(current - 1)
+    }
+
   return (
-    <div className='carousel'>
+    <div className='carousel' 
+            onMouseEnter={() => {
+                setAutoPlay(false)
+                }} 
+            onMouseLeave={() => {
+                setAutoPlay(true)
+                clearTimeout(timeOut)
+                }}>
         <div className='carousel-wrapper'>
         {images.map((image, index) => {
             return (
-                <div key={index} className="carousel-card">
-                    <img src={image.image} alt="" />
-                    <h2 className='carousel-card--title'>
-                        {image.title}
-                    </h2>
+                <div key={ index } className={ index == current
+                        ? "carosel-card carousel-card--active" 
+                        : "carousel-card"
+                        }>
+                        <img src={image.image} className='carousel-card--img' />
+                        <div className="carousel-card--overlay">
+                            <h2 className='carousel-card--title'>
+                                {image.title}
+                            </h2>
+                        </div>
                 </div>
             )
         })}
+        <div className='carousel-arrow carousel-arrow--left' onClick={slideLeft}>&lsaquo;</div>
+        <div className='carousel-arrow carousel-arrow--right' onClick={slideRight}>&rsaquo;</div>
         </div>
     </div>
   )
