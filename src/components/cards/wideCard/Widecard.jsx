e336267ccdb8271454b64576c62984bc523449d1
erikdalgrund <92083787+erikdalgrund@users.noreply.github.com>
2022-12-26 13:17:17 +0100

    examen[013] Chore: created a widecard component

---
+++ src/components/cards/wideCard/Widecard.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Widecard() {
+  return (
+    <div className='widecard'>
+        <div className='widecard-left'> 
+
+        </div>
+        <div className='widecard-right'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Widecard
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,11 +1,9 @@
 import Header from "./header"; 
 import Footer from "./footer";
 import Carousel from "./carousel";
-import CardInfo from "./card";
 
 export {
     Header,
     Footer,
     Carousel,
-    CardInfo
 }
