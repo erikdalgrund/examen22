3473f248f097602e17fa0ddf27b7f385428f9c72
erikdalgrund <92083787+erikdalgrund@users.noreply.github.com>
2022-12-26 12:51:35 +0100

    examen[012] Chore: created a card component with imports

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,9 +1,11 @@
 import Header from "./header"; 
 import Footer from "./footer";
 import Carousel from "./carousel";
+import CardInfo from "./card";
 
 export {
     Header,
     Footer,
     Carousel,
+    CardInfo
 }
