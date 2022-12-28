b31a355e82f94024a04f77289bc490d59732e785
erikdalgrund <92083787+erikdalgrund@users.noreply.github.com>
2022-12-28 12:47:44 +0100

    examen[015] Chore: updated the import / export in pages/index file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import Home from "./homepage";
 import Indoor from "./indoor/Indoor";
 import Outdoor from "./outdoor";
+import Faq from "./f.a.q";
 
 export{
     Home,
     Indoor,
-    Outdoor
+    Outdoor,
+    Faq
 }
