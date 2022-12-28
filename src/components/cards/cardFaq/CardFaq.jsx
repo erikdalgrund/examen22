9f9ea17b2e6887c09c6ffd51cfeae9f5379b0a5e
erikdalgrund <92083787+erikdalgrund@users.noreply.github.com>
2022-12-28 12:46:56 +0100

    examen[015] Chore: Created a card for Faq page

---
+++ src/components/cards/cardFaq/CardFaq.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+function CardFaq() {
+  return (
+    <div className="card-faq">
+        <h2>Interior Paint FAQs</h2>
+        <span className='card-faq--items'>
+        <p>
+            Taking on the task of house painting comes with many decisions. 
+            Choosing paint colors, paint sheens, and hiring professional painters 
+            are just some of the decisions you’ll make when you’re ready to transform 
+            your home with new interior paint. While you can certainly find interior 
+            design ideas in magazines, on the internet, and in other people’s homes, 
+            the dos and don’ts are a little harder to come by.
+        </p>
+        <p>
+            Here are a few Interior Painting FAQs that we at New Life Painting are ready to answer to help get you started.
+        </p>
+        </span>
+        <span className='card-faq--items'>
+            <h2>How do I select an interior paint color?</h2>
+            <p>
+              The answer to this questions could go on for pages, but we will try to keep it simple.
+              The first thing to consider is lighting. A poorly lit room will appear brighter, bigger,
+              and more open when painted a light paint color.
+              Larger rooms can be made to feel a little more cozy when painted a darker paint color.
+              Small rooms will have a more dramatic feel when painted darker colors.
+              Paint colors really have a lot to do with the mood you want to evoke in a room as well as your personal style.
+            </p>  
+        </span>
+        <span className='card-faq--items'>
+            <h2>Should I try out paint colors?</h2>
+            <p>
+              Even if you feel determined that you have chosen the right color, it is worth your
+              time and money to buy a sample and test it out in your room. Paint colors can look
+              drastically different in your home than they will in the paint store or online. Rather
+              than painting small patches all over your room, paint poster board with your
+              chosen colors and tape it to your wall. You can move the poster board around the
+              room in order to see how your color acts in different lighting situations.
+            </p>  
+        </span>
+        <span className='card-faq--items'>
+            <h2>How do I choose the best paint sheen for my paint job?</h2>
+            <p>
+              Choosing a sheen is actually easier than choosing your paint color. Most interior
+              rooms are going to be painted with a satin or eggshell finish. Eggshell is the safe
+              middle ground between high gloss and matte. It creates a smooth finish that is
+              also easy to clean. Smaller areas like doors, trim, and cabinets, can be painted in a
+              high gloss or semi-gloss sheen. These areas are less likely to get dirty and look best
+              with some gloss. Matte sheen is most often used for something like your ceiling.
+              While painting with a matte sheen is easy, it’s not easy to clean, 
+              so it’s better to use on areas with less traffic.
+            </p>  
+        </span>
+        <span className='card-faq--items'>
+            <h2>How do I prep a room for interior painting?</h2>
+            <p>
+              Your first step to prep a room for painting is to remove all of the furniture. If the room is large enough, 
+              furniture can be placed in the center of the room. Next, take
+              everything off of the walls. If there are holes in the walls from art or pictures 
+              that have been hanging, fill in the holes before painting.
+            </p>  
+        </span>
+    </div>
+  )
+}
+
+export default CardFaq
