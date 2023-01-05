21e5c6412c35c18060d0976493ad9dcf35cdb3f2
erikdalgrund <92083787+erikdalgrund@users.noreply.github.com>
2023-01-05 14:09:00 +0100

    examen[021] Chore: A beta version of a register and login form for the project.

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,28 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react';
+import Axios from 'axios';
 
 function Login() {
+
+    // State for register new user
+    const [usernameReq, setUsernameReq] = useState('');
+    const [passwordReq, setPasswordReq] = useState('');
+    const [emailReq, setEmailReq] = useState('');
+
+    // State for login user
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    // State for login message 
+    const [loginStatus, setLoginStatus] = useState('');
+
+    // Function for register new user
+    const register = () => {
+        Axios.post('http://localhost:3001/register', {
+            username: usernameReq,
+            password: passwordReq,
+            email: emailReq
+        }).then((response) => {
+            console.log(response);
+        });
+    }
+
+    // Function for login user
+    const login = () => {
+        Axios.post('http://localhost:3001/login', {
+            username: username,
+            password: password,
+        }).then((response) => {
+            if (response.data.message) {
+                setLoginStatus(response.data.message)
+            } else {
+                setLoginStatus(response.data[0].username)
+            }
+        });
+    }
+
   return (
     <div className='loginPage'>
         <div className="form-wrapper">
             <div className='form-wrapper--login'>
                 <h3> Login </h3>
                 <label> Username </label>
-                <input type="text" />
+                <input 
+                    type="text"
+                    onChange={(e) => {
+                        setUsername(e.target.value)
+                    }}  
+                />
                 <label htmlFor=""> Password </label>
-                <input type="text" />
-                <button> Login </button>
+                <input 
+                    type="text"
+                    onChange={(e) => {
+                        setPassword(e.target.value)
+                    }}  
+                />
+                <button onClick={login}> Login </button>
             </div>
             <div className="form-wrapper--register">
                 <h3> register </h3>
                 <label> Username </label>
-                <input type="text" />
+                <input 
+                    type="text"
+                    onChange={(e) => {
+                        setUsernameReq(e.target.value)
+                    }} 
+                />
                 <label htmlFor=""> Email </label>
-                <input type="text" />
+                <input 
+                    type="text"
+                    onChange={(e) => {
+                        setEmailReq(e.target.value)
+                    }} 
+                />
                 <label htmlFor=""> Password </label>
-                <input type="text" />
-                <button> Register </button>
+                <input 
+                    type="text"
+                    onChange={(e) => {
+                        setPasswordReq(e.target.value)
+                    }} 
+                />
+                <button onClick={register}> Register </button>
             </div>
         </div>
+        <h2>{loginStatus}</h2>
     </div>
   )
 }
