a8173fec1ef8165076f63f2441907aafbfd514bb
erikdalgrund <92083787+erikdalgrund@users.noreply.github.com>
2023-01-10 15:22:35 +0100

    examen[025] Chore: Exporting "Tools"

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,13 @@ import Indoor from "./indoor/Indoor";
 import Outdoor from "./outdoor";
 import Faq from "./f.a.q";
 import Login from "./login";
+import Tools from "./tools";
 
 export{
     Home,
     Indoor,
     Outdoor,
     Faq,
-    Login
+    Login,
+    Tools
 }
