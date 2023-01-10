1ba27b7f85e41f8118d299fdb52ffb3cbe3fe3b8
erikdalgrund <92083787+erikdalgrund@users.noreply.github.com>
2023-01-10 15:23:58 +0100

    examen[025] Chore: A .js file that holds the tool products.

---
+++ src/productData/productTools.js
@@ -0,0 +1,100 @@
+
+// Products Paint brush
+const brushOne = "/images/products/brushes/fasadpensel-vinklad.png";
+const brushTwo = "/images/products/brushes/lackpensel-elite-pro.jpg";
+const brushThree = "/images/products/brushes/lackpensel.png";
+const brushFour = "/images/products/brushes/traoljepensel.png";
+const brushFive = "/images/products/brushes/vinkelpensel-proffs.png";
+const brushSix = "/images/products/brushes/brushes-product.avif";
+
+// Products Roller
+const rollerOne = "/images/products/roller/roller-product.jpeg";
+const rollerTwo = "/images/products/roller/maxibygel-18-ny.png";
+const rollerThree = "/images/products/roller/maxiroller-platinum-medelfin.png";
+
+// Products putty knife 
+const puttyOne = "/images/products/puttyKnife/bredspackel-plast.png";
+const puttyTwo = "/images/products/puttyKnife/bredspackel-tra.png";
+const puttyThree = "/images/products/puttyKnife/japanspackel.png";
+
+// Brushes
+export const paintBrushes = [
+    {
+        id: 1,
+        productName: "Brush one",
+        price: 129.95,
+        image: brushOne
+    },
+    {
+        id: 2,
+        productName: "Brush two",
+        price: 229,
+        image: brushTwo
+    },
+    {
+        id: 3,
+        productName: "Brush three",
+        price: 159,
+        image: brushThree
+    },
+    {
+        id: 4, 
+        productName: "Brush four",
+        price: 129.95,
+        image: brushFour
+    },
+    {
+        id: 5,
+        productName: "Brush five",
+        price: 159,
+        image: brushFive
+    },
+    {
+        id: 6,
+        productName: "Brush set",
+        price: 399,
+        image: brushSix
+    },
+]
+
+export const rollers = [
+    {
+        id: 7,
+        productName: "Roller set",
+        price: 399,
+        image: rollerOne
+    },
+    {
+        id: 8,
+        productName: "Maxibygel",
+        price: 129,
+        image: rollerTwo
+    },
+    {
+        id: 9,
+        productName: "Roller new",
+        price: 99,
+        image: rollerThree
+    },
+]
+
+export const puttyKnife = [
+    {
+        id: 10,
+        productName: "Broad putty plastic",
+        price: 229,
+        image: puttyOne
+    },
+    {
+        id: 11,
+        productName: "Broad putty wood",
+        price: 129,
+        image: puttyTwo
+    },
+    {
+        id: 12,
+        productName: "Japan putty",
+        price: 199,
+        image: puttyThree
+    },
+]
