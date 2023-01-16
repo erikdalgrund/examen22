86f89d7ca26fbe02f70ef5ba4472aa395b122857
erikdalgrund <92083787+erikdalgrund@users.noreply.github.com>
2023-01-16 17:07:39 +0100

    examen[032] Chore: for the new indoor paint shop page.

--- src/pages/indoor/Indoor.jsx
+++ src/pages/indoor/Indoor.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { CardImageText } from '../../components/cards';
+import { IndoorPaint } from '../../components/products';
+import { indoorColorArr } from '../../productData/productIndoorColor';
 
 function Indoor() {
   return (
@@ -11,6 +13,16 @@ function Indoor() {
           title="Paint indoor"
           content="Here you will find all the colors you can imagine to spice up your room"
           />
+      </div>
+      <div className='tools__allTools'>
+          <div className='tools__title'>
+              <h3>Indoor Paint</h3>
+          </div>
+          <div className='brush__wrapper'>
+              {indoorColorArr.map((paint) => {
+                  return <IndoorPaint key={paint.id} data={paint} />
+              })}
+          </div>
       </div>
     </div>
   )
