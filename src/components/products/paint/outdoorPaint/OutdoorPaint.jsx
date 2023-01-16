be5bcba2584d619714c48a34d29fae32158f5de8
erikdalgrund <92083787+erikdalgrund@users.noreply.github.com>
2023-01-16 18:18:37 +0100

    examen[033] Chore: Added an outdoorPaint component

---
+++ src/components/products/paint/outdoorPaint/OutdoorPaint.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react'
+import { ShopContext } from '../../../shoppingCart/context/ShopContextProvider'
+
+function OutdoorPaint(props) {
+
+    const { id, productName, price, image} = props.data;
+    const { addToCart, CartItems } = useContext(ShopContext);
+    const cartItemAmount = CartItems[id];
+
+  return (
+    <div className='tool'>
+        <h3 className='tool__title'>{productName}</h3>
+        <div className='tool__image'>
+            <img src={image} />
+        </div>
+        <p className='tool__price'> ${price}</p>
+        <button 
+            className='tool__addToCart' 
+            onClick={() => addToCart(id)}>Add to cart {cartItemAmount >  0 && <> ({cartItemAmount}) </>}
+        </button>
+    </div>  
+  )
+}
+
+export default OutdoorPaint
