d5db1dffd5f75f653db289262686874d30d72902
erikdalgrund <92083787+erikdalgrund@users.noreply.github.com>
2023-01-16 19:06:06 +0100

    examen[034] Chore: changed some structure in the footer component

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -4,22 +4,35 @@ import {FaFacebook, FaInstagram, FaTwitter} from "react-icons/fa";
 function Footer() {
     return (
         <div className="footer">
-            <div className="footer-partners">
-                <p>Our partners:</p>
-                <div className="footer-images">
-                    <img src="/images/footer/casadeco.jpg" />
-                    <img src="/images/footer/dekora.jpg" />
-                    <img src="/images/footer/pergo.jpg" />
-                    <img src="/images/footer/forbo.jpg" />
+            <div className="footer-info">
+                <div className="footer-partners">
+                    <div className="footer-images">
+                        <p>Proud partners with</p>
+                        <img src="/images/footer/casadeco.jpg" />
+                        <img src="/images/footer/dekora.jpg" />
+                        <img src="/images/footer/pergo.jpg" />
+                        <img src="/images/footer/forbo.jpg" />
+                    </div>
                 </div>
-            </div>
-            <div className="footer-follow">
-                <ul className="footer-follow--icons">
-                    <li><p>Follow us!</p></li>
-                    <li><FaFacebook className="icon"/></li>
-                    <li><FaInstagram className="icon"/></li>
-                    <li><FaTwitter className="icon"/></li>
-                </ul>
+                <div className="footer-aboutUs">
+                    <h3>Color Code</h3>
+                    <p>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                        Facilis soluta placeat quibusdam odit quis quos rerum eaque, 
+                        laboriosam totam culpa, at repellat quas harum magnam quasi 
+                        tempora animi ut voluptates obcaecati, molestias expedita sunt.
+                        Animi molestiae inventore veniam, optio voluptate expedita ipsa
+                        accusantium nam omnis qui fugiat, magni sapiente iusto.
+                    </p>
+                </div>
+                <div className="footer-follow">
+                    <ul className="footer-follow--icons">
+                        <li><p>Follow us!</p></li>
+                        <li><FaFacebook className="icon"/></li>
+                        <li><FaInstagram className="icon"/></li>
+                        <li><FaTwitter className="icon"/></li>
+                    </ul>
+                </div>
             </div>
             <div className="footer-bottom">
                 <p className="footer-bottom--title">Color code</p>
